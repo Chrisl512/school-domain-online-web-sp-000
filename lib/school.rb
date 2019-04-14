dd2d3e71284f799670bbd68f0d07440db0743ca2
Christopher Luna <chris@illusionlayervfx.com>
2019-04-14 02:06:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,10 +11,10 @@ end
 
 def add_student(name, grade)
   roster[grade] = []
-  if roster.include?(grade)
-  roster[grade] << name
-  else
- end
+    if roster.include?(grade)
+      roster[grade] << name
+    else
+  end
 end
 
 end
