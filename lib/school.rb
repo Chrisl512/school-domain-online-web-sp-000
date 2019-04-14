ad00094ad1ba2a2a08ec3e1b8b03d7c37a030645
Christopher Luna <chris@illusionlayervfx.com>
2019-04-14 01:56:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :name
+  attr_reader :roster
+
+def initialize(roster)
+  roster = Hash.new([])
+  @roster = roster
+end
+
+def add_student(name, grade)
+  roster[grade] = []
+  roster[grade] << name
+  
+end
+
+end
